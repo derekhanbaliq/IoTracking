3ac23dbd0458ce486b2e7171bbac53c5c414821f
weihh131 <42785787+weihh131@users.noreply.github.com>
2022-03-31 12:15:46 -0400

    created separate branch

---
+++ A8/A8_Iotracking/WINC1500_HTTP_DOWNLOADER_EXAMPLE1/src/UiHandlerThread/UiHandlerThread.c
@@ -0,0 +1,98 @@
+ /**************************************************************************//**
+* @file      UiHandlerThread.c
+* @brief     File that contains the task code and supporting code for the UI Thread for ESE516 Spring (Online) Edition
+* @author    You! :)
+* @date      2020-04-09 
+
+******************************************************************************/
+
+
+/******************************************************************************
+* Includes
+******************************************************************************/
+#include <errno.h>
+#include "asf.h"
+#include "UiHandlerThread/UiHandlerThread.h"
+#include "SeesawDriver/Seesaw.h"
+#include "SerialConsole.h"
+#include "main.h"
+
+/******************************************************************************
+* Defines
+******************************************************************************/
+
+/******************************************************************************
+* Variables
+******************************************************************************/
+uiStateMachine_state uiState;
+/******************************************************************************
+* Forward Declarations
+******************************************************************************/
+
+/******************************************************************************
+* Callback Functions
+******************************************************************************/
+
+
+/******************************************************************************
+* Task Function
+******************************************************************************/
+
+/**************************************************************************//**
+* @fn		void vUiHandlerTask( void *pvParameters )
+* @brief	STUDENT TO FILL THIS
+* @details 	student to fill this
+                				
+* @param[in]	Parameters passed when task is initialized. In this case we can ignore them!
+* @return		Should not return! This is a task defining function.
+* @note         
+*****************************************************************************/
+void vUiHandlerTask( void *pvParameters )
+{
+//Do initialization code here
+SerialConsoleWriteString("UI Task Started!");
+uiState = UI_STATE_HANDLE_BUTTONS;
+
+//Here we start the loop for the UI State Machine
+while(1)
+{
+	switch(uiState)
+	{
+		case(UI_STATE_HANDLE_BUTTONS):
+		{
+		//Do the handle buttons code here!
+
+		break;
+		}
+
+		case(UI_STATE_IGNORE_PRESSES):
+		{
+		//Ignore me for now
+			break;
+		}
+
+		case(UI_STATE_SHOW_MOVES):
+		{
+		//Ignore me as well
+			break;
+		}
+
+		default: //In case of unforseen error, it is always good to sent state machine to an initial state.
+			uiState = UI_STATE_HANDLE_BUTTONS;
+		break;
+	}
+
+	//After execution, you can put a thread to sleep for some time.
+	vTaskDelay(50);
+}
+
+
+
+}
+
+
+
+
+/******************************************************************************
+* Functions
+******************************************************************************/
